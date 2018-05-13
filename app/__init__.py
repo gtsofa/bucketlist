2f473af310b63d53b96a08cebab66b9217ac257e
gtsofa <jlsnyule@yahoo.com>
2018-05-13 21:47:17 +0300

    add dependencies for python3

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ from instance.config import app_config
 db = SQLAlchemy()
 
 def create_app(config_name):
-    app = FlaskAPI(__name__, instance_relativie_config=True)
+    app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
