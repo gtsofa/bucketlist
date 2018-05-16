6c85ca8d1e560e3c87e39ddadc57e9e64d660609
gtsofa <jlsnyule@yahoo.com>
2018-05-16 19:52:54 +0300

    add functionality code for GET and POST tests

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
 #from flask_migrate import Migrate
 #from app import models
+from flask import request, jsonify, abort
 
 
 # local imports
@@ -13,6 +14,9 @@ from instance.config import app_config
 db = SQLAlchemy()
 
 def create_app(config_name):
+    from api.models import Bucketlist
+
+
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
@@ -26,6 +30,39 @@ def create_app(config_name):
     # @app.route('/')
     # def hello_world():
     #     return 'Hello world guys'
+
+    @app.route('/bucketlists/', methods=['POST', 'GET'])
+    def bucketlists():
+        if request.method == 'POST':
+            name = str(request.data.get('name', ''))
+            if name:
+                bucketlist = Bucketlist(name=name)
+                bucketlist.save()
+                response = jsonify({
+                    'id': bucketlist.id,
+                    'name': bucketlist.name
+                    'date_created': bucketlist.date_created,
+                    'date_modified': bucketlist.date_modified
+                })
+                response.status_code = 201
+                return response
+
+        else:
+            # GET
+            bucketlists = Bucketlist.get_all()
+            results = []
+
+            for bucketlist in bucketlists:
+                obj = {
+                    'id': bucketlist.id,
+                    'name': bucketlist.name,
+                    'date_created': bucketlist.date_created,
+                    'date_modified': bucketlist.date_modified
+                }
+                results.append(obj)
+            response = jsonify(results)
+            response.status_code = 200
+            return response
 
     return app
 
